2fb76b90c133f83d44894957e968ecb4fb4953fd
j <j@j.com>
2017-04-24 01:15:54 +1000

    Removed devtools for redux

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -3,10 +3,15 @@ import reducers from './index';
 import thunk from 'redux-thunk';
 import { compose, applyMiddleware } from 'redux';
 
+const isDev =false;
+var useReduxDevTools=null;
+if(isDev){
+   useReduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+}
+
 const store = createStore(reducers, 
     compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(thunk)
     )
 );
 export default store;
